83e7f0ad8806ea28ecc25eec1f65f7a712b1af29
Faruman <fabian.karst@student.unisg.ch>
2020-05-04 20:44:14 +0200

    Updated example sql script

--- sql/sql_test.R
+++ sql/sql_test.R
@@ -5,6 +5,6 @@ library(RMariaDB)
 bigdatadb <- dbConnect(RMariaDB::MariaDB(), user='bdauser', password='BDA4ever!!!', dbname='bigdatadb', host='35.193.193.138')
 dbListTables(bigdatadb)
 
-query = "SELECT * FROM test"
-result = dbSendQuery(bigdatadb,query)
+query = "SELECT * FROM openaq_state WHERE openaq_state.country= 'AU';"
+result = dbGetQuery(bigdatadb,query)
 
